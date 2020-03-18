147cd6c02d20b98f2b69c7ce9d63bfcbac779078
Alfe25 <47866847+Alfe25@users.noreply.github.com>
2020-03-18 20:51:33 +0100

    Inicio del trabajo

---
+++ subsumptionlightcontroller.cpp
@@ -0,0 +1,325 @@
+/******************* INCLUDES ******************/
+/***********************************************/
+
+/******************** General ******************/
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <sys/time.h>
+#include <iostream>
+
+/******************** Simulator ****************/
+/******************** Sensors ******************/
+#include "epuckproximitysensor.h"
+#include "contactsensor.h"
+#include "lightsensor.h"
+#include "groundsensor.h"
+#include "groundmemorysensor.h"
+#include "batterysensor.h"
+
+/******************** Actuators ****************/
+#include "wheelsactuator.h"
+
+/******************** Controller **************/
+#include "subsumptionlightcontroller.h"
+
+
+/******************************************************************************/
+/******************************************************************************/
+
+extern gsl_rng* rng;
+extern long int rngSeed;
+/******************************************************************************/
+/******************************************************************************/
+
+using namespace std;
+/******************************************************************************/
+/******************************************************************************/
+
+#define BEHAVIORS	3
+
+#define AVOID_PRIORITY 		0
+#define RELOAD_PRIORITY 	1
+#define NAVIGATE_PRIORITY 2
+
+#define PROXIMITY_THRESHOLD 0.3
+#define BATTERY_THRESHOLD 0.5
+
+#define SPEED 500.0
+
+
+/******************************************************************************/
+/******************************************************************************/
+CSubsumptionLightController::CSubsumptionLightController (const char* pch_name, CEpuck* pc_epuck, int n_write_to_file) : CController (pch_name, pc_epuck)
+
+{
+	/* Set Write to File */
+	m_nWriteToFile = n_write_to_file;
+	
+	/* Set epuck */
+	m_pcEpuck = pc_epuck;
+	/* Set Wheels */
+	m_acWheels = (CWheelsActuator*) m_pcEpuck->GetActuator(ACTUATOR_WHEELS);
+	/* Set Prox Sensor */
+	m_seProx = (CEpuckProximitySensor*) m_pcEpuck->GetSensor(SENSOR_PROXIMITY);
+	/* Set light Sensor */
+	m_seLight = (CLightSensor*) m_pcEpuck->GetSensor(SENSOR_LIGHT);
+	/* Set contact Sensor */
+	m_seContact = (CContactSensor*) m_pcEpuck->GetSensor (SENSOR_CONTACT);
+	/* Set ground Sensor */
+	m_seGround = (CGroundSensor*) m_pcEpuck->GetSensor (SENSOR_GROUND);
+	/* Set ground memory Sensor */
+	m_seGroundMemory = (CGroundMemorySensor*) m_pcEpuck->GetSensor (SENSOR_GROUND_MEMORY);
+	/* Set battery Sensor */
+	m_seBattery = (CBatterySensor*) m_pcEpuck->GetSensor (SENSOR_BATTERY);
+
+	
+	/* Initilize Variables */
+	m_fLeftSpeed = 0.0;
+	m_fRightSpeed = 0.0;
+
+
+	/* Create TABLE for the COORDINATOR */
+	m_fActivationTable = new double* [BEHAVIORS];
+	for ( int i = 0 ; i < BEHAVIORS ; i++ )
+	{
+		m_fActivationTable[i] = new double[3];
+	}
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+CSubsumptionLightController::~CSubsumptionLightController()
+{
+	for ( int i = 0 ; i < BEHAVIORS ; i++ )
+	{
+		delete [] m_fActivationTable;
+	}
+}
+
+
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightController::SimulationStep(unsigned n_step_number, double f_time, double f_step_interval)
+{
+
+	/* Move time to global variable, so it can be used by the bahaviors to write to files*/
+	m_fTime = f_time;
+
+	/* Execute the levels of competence */
+	ExecuteBehaviors();
+
+	/* Execute Coordinator */
+	Coordinator();
+
+	/* Set Speed to wheels */
+	m_acWheels->SetSpeed(m_fLeftSpeed, m_fRightSpeed);
+
+	if (m_nWriteToFile ) 
+	{
+	/* INIT: WRITE TO FILES */
+	/* Write robot position and orientation */
+		FILE* filePosition = fopen("outputFiles/robotPosition", "a");
+		fprintf(filePosition,"%2.4f %2.4f %2.4f %2.4f\n", m_fTime, m_pcEpuck->GetPosition().x, m_pcEpuck->GetPosition().y, m_pcEpuck->GetRotation());
+		fclose(filePosition);
+
+		/* Write robot wheels speed */
+		FILE* fileWheels = fopen("outputFiles/robotWheels", "a");
+		fprintf(fileWheels,"%2.4f %2.4f %2.4f \n", m_fTime, m_fLeftSpeed, m_fRightSpeed);
+		fclose(fileWheels);
+		/* END WRITE TO FILES */
+	}
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightController::ExecuteBehaviors ( void )
+{
+	for ( int i = 0 ; i < BEHAVIORS ; i++ )
+	{
+		m_fActivationTable[i][2] = 0.0;
+	}
+	/* Set Leds to BLACK */
+	m_pcEpuck->SetAllColoredLeds(	LED_COLOR_BLACK);
+	
+	ObstacleAvoidance ( AVOID_PRIORITY );
+  GoLoad ( RELOAD_PRIORITY );
+	Navigate ( NAVIGATE_PRIORITY );
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightController::Coordinator ( void )
+{
+	int nBehavior;
+	for ( nBehavior = 0 ; nBehavior < BEHAVIORS ; nBehavior++ )
+	{
+		if ( m_fActivationTable[nBehavior][2] == 1.0 )
+		{
+			break;
+		}
+	}
+
+	m_fLeftSpeed = m_fActivationTable[nBehavior][0];
+	m_fRightSpeed = m_fActivationTable[nBehavior][1];
+	
+  printf("%d %2.4f %2.4f \n", nBehavior, m_fLeftSpeed, m_fRightSpeed);
+	
+  if (m_nWriteToFile ) 
+	{
+		/* INIT: WRITE TO FILES */
+		/* Write coordinator ouputs */
+		FILE* fileOutput = fopen("outputFiles/coordinatorOutput", "a");
+		fprintf(fileOutput,"%2.4f %d %2.4f %2.4f \n", m_fTime, nBehavior, m_fLeftSpeed, m_fRightSpeed);
+		fclose(fileOutput);
+		/* END WRITE TO FILES */
+	}
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightController::ObstacleAvoidance ( unsigned int un_priority )
+{
+	
+	/* Leer Sensores de Proximidad */
+	double* prox = m_seProx->GetSensorReading(m_pcEpuck);
+
+	double fMaxProx = 0.0;
+	const double* proxDirections = m_seProx->GetSensorDirections();
+
+	dVector2 vRepelent;
+	vRepelent.x = 0.0;
+	vRepelent.y = 0.0;
+
+	/* Calc vector Sum */
+	for ( int i = 0 ; i < m_seProx->GetNumberOfInputs() ; i ++ )
+	{
+		vRepelent.x += prox[i] * cos ( proxDirections[i] );
+		vRepelent.y += prox[i] * sin ( proxDirections[i] );
+
+		if ( prox[i] > fMaxProx )
+			fMaxProx = prox[i];
+	}
+	
+	/* Calc pointing angle */
+	float fRepelent = atan2(vRepelent.y, vRepelent.x);
+	/* Create repelent angle */
+	fRepelent -= M_PI;
+	/* Normalize angle */
+	while ( fRepelent > M_PI ) fRepelent -= 2 * M_PI;
+	while ( fRepelent < -M_PI ) fRepelent += 2 * M_PI;
+
+
+	/* If above a threshold */
+	if ( fMaxProx > PROXIMITY_THRESHOLD )
+	{
+		/* Set Leds to GREEN */
+		m_pcEpuck->SetAllColoredLeds(	LED_COLOR_GREEN);
+
+
+		double fCLinear = 1.0;
+		double fCAngular = 1.0;
+		double fC1 = SPEED / M_PI;
+		
+		/* Calc Linear Speed */
+		double fVLinear = SPEED * fCLinear * ( cos ( fRepelent / 2) );
+
+		/*Calc Angular Speed */
+		double fVAngular = fRepelent;
+
+		m_fActivationTable[un_priority][0] = fVLinear - fC1 * fVAngular;
+		m_fActivationTable[un_priority][1] = fVLinear + fC1 * fVAngular;
+		m_fActivationTable[un_priority][2] = 1.0;
+	}
+	
+	if (m_nWriteToFile ) 
+	{
+		/* INIT WRITE TO FILE */
+		/* Write level of competence ouputs */
+		FILE* fileOutput = fopen("outputFiles/avoidOutput", "a");
+		fprintf(fileOutput, "%2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f ", m_fTime, prox[0], prox[1], prox[2], prox[3], prox[4], prox[5], prox[6], prox[7], fMaxProx, fRepelent);
+		fprintf(fileOutput, "%2.4f %2.4f %2.4f\n",m_fActivationTable[un_priority][2], m_fActivationTable[un_priority][0], m_fActivationTable[un_priority][1]);
+		fclose(fileOutput);
+		/* END WRITE TO FILE */
+	}
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightController::Navigate ( unsigned int un_priority )
+{
+	m_fActivationTable[un_priority][0] = SPEED;
+	m_fActivationTable[un_priority][1] = SPEED;
+	m_fActivationTable[un_priority][2] = 1.0;
+
+	if (m_nWriteToFile ) 
+	{
+		/* INIT: WRITE TO FILES */
+		/* Write level of competence ouputs */
+		FILE* fileOutput = fopen("outputFiles/navigateOutput", "a");
+		fprintf(fileOutput,"%2.4f %2.4f %2.4f %2.4f \n", m_fTime, m_fActivationTable[un_priority][2], m_fActivationTable[un_priority][0], m_fActivationTable[un_priority][1]);
+		fclose(fileOutput);
+		/* END WRITE TO FILES */
+	}
+}
+		
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightController::GoLoad ( unsigned int un_priority )
+{
+	/* Leer Battery Sensores */
+	double* battery = m_seBattery->GetSensorReading(m_pcEpuck);
+
+	/* Leer Sensores de Luz */
+	double* light = m_seLight->GetSensorReading(m_pcEpuck);
+
+
+	/* If battery below a BATTERY_THRESHOLD */
+	if ( battery[0] < BATTERY_THRESHOLD )
+	{
+		/* Set Leds to RED */
+		m_pcEpuck->SetAllColoredLeds(	LED_COLOR_RED);
+		
+
+		/* If not pointing to the light */
+		if ( ( light[0] * light[7] == 0.0 ) )
+		{
+			/* Activate level of competence */
+			m_fActivationTable[un_priority][2] = 1.0;
+
+			/* Calc light intensity at the left and right */
+			double lightLeft 	= light[0] + light[1] + light[2] + light[3];
+			double lightRight = light[4] + light[5] + light[6] + light[7];
+
+			/* If light on the left */
+			if ( lightLeft > lightRight )
+			{
+				/* Turn left */
+				m_fActivationTable[un_priority][0] = -SPEED;
+				m_fActivationTable[un_priority][1] = SPEED;
+			}
+			else
+			{
+				/* Turn right */
+				m_fActivationTable[un_priority][0] = SPEED;
+				m_fActivationTable[un_priority][1] = -SPEED;
+			}
+		}
+	}	
+
+	if (m_nWriteToFile ) 
+	{
+		/* INIT WRITE TO FILE */
+		FILE* fileOutput = fopen("outputFiles/batteryOutput", "a");
+		fprintf(fileOutput, "%2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f ", m_fTime, battery[0], light[0], light[1], light[2], light[3], light[4], light[5], light[6], light[7]);
+		fprintf(fileOutput, "%2.4f %2.4f %2.4f\n",m_fActivationTable[un_priority][2], m_fActivationTable[un_priority][0], m_fActivationTable[un_priority][1]);
+		fclose(fileOutput);
+		/* END WRITE TO FILE */
+	}
+}
---
+++ subsumptionlightexp.cpp
@@ -0,0 +1,312 @@
+
+/******************* INCLUDES ******************/
+/***********************************************/
+
+/******************** General ******************/
+
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <sys/time.h>
+
+/******************** Simulator ****************/
+#include <vector>
+#include "simmath.h"
+#include "random.h"
+#include "programmedarena.h"
+
+#include "subsumptionlightexp.h"
+
+/******************** Sensors ******************/
+#include "contactsensor.h"
+#include "epuckproximitysensor.h"
+#include "lightsensor.h"
+#include "groundsensor.h"
+#include "groundmemorysensor.h"
+#include "batterysensor.h"
+
+/******************** Actuators ****************/
+#include "wheelsactuator.h"
+
+/******************** Controllers **************/
+#include "subsumptionlightcontroller.h"
+
+using namespace std;
+
+/*Create Arena */
+static char* pchHeightMap = 
+"%%%%%%%%%%%%%%%%%%%%"
+"%##################%"
+"%######%%%#########%"
+"%##################%"
+"%##################%"
+"%##################%"
+"%############%%%###%"
+"%##################%"
+"%##%%%%############%"
+"%##################%"
+"%##################%"
+"%##########%%%%%###%"
+"%##################%"
+"%##################%"
+"%##################%"
+"%##################%"
+"%###%%%#####%%%%###%"
+"%##################%"
+"%##################%"
+"%%%%%%%%%%%%%%%%%%%%";
+
+
+extern gsl_rng* rng;
+extern long int rngSeed;
+
+/*******************************************************************************/
+///*******************************************************************************/
+//
+CSubsumptionLightExp::CSubsumptionLightExp(const char* pch_name, const char* paramsFile) :
+	CExperiment(pch_name, COLLISION_MODEL_SIMPLE, COLLISION_HANDLER_POSITION)
+{
+	
+	/* If there is not a parameter file input get default values*/
+	if (paramsFile == NULL )
+	{
+		m_nRobotsNumber = 1;
+		SetNumberOfEpucks(m_nRobotsNumber);
+		m_nRunTime = 10000;;
+		m_pcvRobotPositions = new dVector2[m_nRobotsNumber];
+		m_fRobotOrientations = new double[m_nRobotsNumber];
+		for ( int i = 0 ; i < m_nRobotsNumber ; i++)
+		{
+			m_pcvRobotPositions[i].x 	= 0.0;
+			m_pcvRobotPositions[i].y 	= 0.0;
+			m_fRobotOrientations[i] 	= 0.0;
+		}
+	
+		m_fLightSensorRange = 1.0; //1 meter
+
+		m_nNumberOfLightObject = 0;
+		m_pcvLightObjects = new dVector2[m_nNumberOfLightObject];
+	
+		m_nNumberOfGroundArea = 0;
+		m_vGroundAreaCenter = new dVector2[m_nNumberOfGroundArea];
+		m_fGroundAreaExternalRadius = new double[m_nNumberOfGroundArea];
+		m_fGroundAreaInternalRadius = new double[m_nNumberOfGroundArea];
+		m_fGroundAreaColor = new double[m_nNumberOfGroundArea];
+	}
+	/* Else, extract info from the file */
+	else
+	{
+		ifstream pfile(paramsFile);
+		if(!pfile) {
+			cerr << "Can't find parameters file " << endl;
+			exit(0);
+		}	
+
+		/* EXTRA */
+		
+		/* Get number of robots */
+		m_nRobotsNumber = getInt('=',pfile);
+		/* Set numer of robots */
+		SetNumberOfEpucks(m_nRobotsNumber);
+		/* For each robot get position and orientation */
+		m_pcvRobotPositions = new dVector2[m_nRobotsNumber];
+		m_fRobotOrientations = new double[m_nRobotsNumber];
+		for ( int i = 0 ; i < m_nRobotsNumber ; i++)
+		{
+			m_pcvRobotPositions[i].x 	= getDouble('=',pfile);
+			m_pcvRobotPositions[i].y 	= getDouble('=',pfile);
+			m_fRobotOrientations[i] 	= getDouble('=',pfile);
+		}
+		
+		/* Get write to file flag */
+		m_nWriteToFile 	= getInt('=',pfile);
+		/* Get Run Time */
+		m_nRunTime = getInt('=',pfile);
+
+		/* ENVIRONMENT */
+		
+		/* Lights */
+		/* Get Light Objects Number */
+		m_nNumberOfLightObject = getInt('=',pfile);
+		/* Create Objects */
+		m_pcvLightObjects = new dVector2[m_nNumberOfLightObject];
+		for ( int i = 0 ; i < m_nNumberOfLightObject; i++){
+			/* Get X position */
+			m_pcvLightObjects[i].x = getDouble('=',pfile);
+			/* Get Y Position */
+			m_pcvLightObjects[i].y = getDouble('=',pfile);
+		}
+
+		/* Ground Areas */
+		/* Get GroundArea Objects */
+		m_nNumberOfGroundArea = getInt('=',pfile);
+		m_vGroundAreaCenter = new dVector2[m_nNumberOfGroundArea];
+		m_fGroundAreaExternalRadius = new double[m_nNumberOfGroundArea];
+		m_fGroundAreaInternalRadius = new double[m_nNumberOfGroundArea];
+		m_fGroundAreaColor = new double[m_nNumberOfGroundArea];
+
+		for ( int i = 0 ; i < m_nNumberOfGroundArea ; i++)
+		{
+			m_vGroundAreaCenter[i].x = getDouble('=',pfile);
+			m_vGroundAreaCenter[i].y = getDouble('=',pfile);
+			m_fGroundAreaInternalRadius[i] = 0.0;
+			m_fGroundAreaExternalRadius[i] = getDouble('=',pfile);
+			m_fGroundAreaColor[i] = getDouble('=',pfile);
+
+		}
+		
+		
+		/* SENSORS */
+		/* Get Light Range */
+
+		m_fLightSensorRange = getDouble('=',pfile); 
+		
+		/* Get Battery load range */
+		m_fBatterySensorRange = getDouble('=',pfile);
+		/* Get batttery charge coef */
+		m_fBatteryChargeCoef = getDouble('=',pfile);
+		/* Get batttery charge coef */
+		m_fBatteryDischargeCoef = getDouble('=',pfile);
+		
+		/* MORPHOLOGY */
+	}
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+CSubsumptionLightExp::~CSubsumptionLightExp ( void )
+{
+	delete [] m_pcvLightObjects;
+	delete [] m_vGroundAreaCenter;
+	delete [] m_fGroundAreaExternalRadius;
+	delete [] m_fGroundAreaInternalRadius;
+	delete [] m_fGroundAreaColor;
+}
+
+	/******************************************************************************/
+/******************************************************************************/
+CArena* CSubsumptionLightExp::CreateArena()
+{
+	/* Create Arena */
+	CArena* pcArena = NULL;
+	pcArena = new CProgrammedArena("CProgrammedArena", 20, 20, 3.0, 3.0);
+	((CProgrammedArena*)pcArena)->SetHeightPixelsFromChars(pchHeightMap, ' ', '#', '%');
+
+	/* Create and add Light Object */
+	char pchTemp[128];
+	CLightObject* pcLightObject = NULL;
+	for( int i = 0 ; i < m_nNumberOfLightObject ; i++){
+		sprintf(pchTemp, "LightObject%d", i);
+		CLightObject* pcLightObject = new CLightObject (pchTemp);
+		pcLightObject->SetCenter(m_pcvLightObjects[i]);
+		pcArena->AddLightObject(pcLightObject);
+	}
+
+	/* Create GroundArea */
+	char sGroundAreaName[100]="epuck";
+
+
+	for ( int i = 0 ; i < m_nNumberOfGroundArea ; i++)
+	{
+		//Create GroundArea
+		sprintf(sGroundAreaName,"groundArea%d",i);
+		CGroundArea* groundArea = new CGroundArea(sGroundAreaName);
+		groundArea->SetCenter(m_vGroundAreaCenter[i]);
+		groundArea->SetExtRadius(m_fGroundAreaExternalRadius[i]);
+		groundArea->SetIntRadius(m_fGroundAreaInternalRadius[i]);
+		groundArea->SetColor(m_fGroundAreaColor[i]);
+		groundArea->SetHeight(0.20);
+		pcArena->AddGroundArea(groundArea);
+	}
+
+	return pcArena;
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightExp::AddActuators(CEpuck* pc_epuck)
+{
+	/* Create and Add Wheels */
+	char pchTemp[128];
+	sprintf(pchTemp, "actuators_%s", pc_epuck->GetName());
+	CActuator* pcActuator = NULL;
+	pcActuator = new CWheelsActuator(pchTemp, pc_epuck);
+	pc_epuck->AddActuator(pcActuator);
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightExp::AddSensors(CEpuck* pc_epuck)
+{
+	//
+	/* Create and add Proximity Sensor */
+	CSensor* pcProxSensor = NULL;
+	pcProxSensor = new CEpuckProximitySensor(252);
+	pc_epuck->AddSensor(pcProxSensor);
+
+	//Light Sensor
+	CSensor* pcLightSensor = NULL;
+	pcLightSensor = new CLightSensor("Light Sensor", m_fLightSensorRange);
+	pc_epuck->AddSensor(pcLightSensor);
+	
+	//Contact Sensor
+	CSensor* pcContactSensor = NULL;
+	pcContactSensor = new CContactSensor("Contact Sensor");
+	pc_epuck->AddSensor(pcContactSensor);
+	
+	//Ground Sensor
+	CSensor* pcGroundSensor = NULL;
+	pcGroundSensor = new CGroundSensor("Ground Sensor");
+	pc_epuck->AddSensor(pcGroundSensor);
+	
+	//Ground Memory Sensor
+	CSensor* pcGroundMemorySensor = NULL;
+	pcGroundMemorySensor = new CGroundMemorySensor("Ground Memory Sensor");
+	pc_epuck->AddSensor(pcGroundMemorySensor);
+	
+	//Battery Sensor
+	CSensor* pcBatterySensor = NULL;
+	pcBatterySensor = new CBatterySensor("Battery Sensor", m_fBatterySensorRange, m_fBatteryChargeCoef, m_fBatteryDischargeCoef);
+	pc_epuck->AddSensor(pcBatterySensor);
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightExp::SetController(CEpuck* pc_epuck)
+{
+	char pchTemp[128];
+	sprintf(pchTemp, "Iri1");
+	CController* pcController = new CSubsumptionLightController(pchTemp, pc_epuck, m_nWriteToFile);
+	pc_epuck->SetControllerType( CONTROLLER_SUBSUMPTION_LIGHT );
+	pc_epuck->SetController(pcController);
+
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightExp::CreateAndAddEpucks(CSimulator* pc_simulator)
+{
+	/* Create and add epucks */
+	char label[100] = "epuck";    
+	for (int i = 0; i < m_nRobotsNumber; i++)
+	{
+		sprintf(label, "epuck%0.4d", i);
+		CEpuck* pcEpuck = CreateEpuck(label, m_pcvRobotPositions[i].x, m_pcvRobotPositions[i].y, m_fRobotOrientations[i]);
+		pc_simulator->AddEpuck(pcEpuck);
+		pc_simulator->SetTimeLimit(m_nRunTime);
+	}
+
+	Reset();
+}
+
+
+/******************************************************************************/
+/******************************************************************************/
+
+void CSubsumptionLightExp::Reset ( void )
+{
+}
